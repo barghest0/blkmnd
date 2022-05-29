e694d9949388eca22b52fae5bb48708829b72e8c
Andrey Mironov <bgmir213@gmail.com>
2022-05-29 14:58:16 +0300

    Аутентификатор

--- php/controllers/api/BaseController.php
+++ php/controllers/api/BaseController.php
@@ -3,10 +3,21 @@
 namespace app\controllers\api;
 
 use Yii;
+use yii\filters\auth\HttpBearerAuth;
+use yii\helpers\ArrayHelper;
 use yii\rest\Controller;
 
 class BaseController extends Controller
 {
+    public function behaviors()
+    {
+        return ArrayHelper::merge(parent::behaviors(), [
+            'authenticator' => [
+                'class' => HttpBearerAuth::className(),
+                'except' => ['error', 'register', 'login'],
+            ],
+        ]);
+    }
     public function actionError()
     {
         return Yii::$app->getErrorHandler();
