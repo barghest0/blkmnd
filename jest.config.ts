7c3b46c4d8aae20da311e19f6516ee7068731ed3
barghest <bolshov03@mail.ru>
2022-06-10 14:55:20 +0000

    Passed the first test

--- jest.config.ts
+++ jest.config.ts
@@ -3,4 +3,18 @@ export default {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
+  rootDir: './src',
+  moduleNameMapper: {
+    '^shared(.*)$': '<rootDir>/shared/$1',
+    '^components(.*)$': '<rootDir>/components/$1',
+    '^hooks(.*)$': '<rootDir>/hooks/$1',
+    '^reduxStore(.*)$': '<rootDir>/redux/$1',
+  },
+  "setupFilesAfterEnv": [
+    "<rootDir>/test-utils/setupTests.ts"
+  ],
+  transform: {
+    '^.+\\.svg$': 'jest-transform-stub',
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+  },
 };
