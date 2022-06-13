194758b6a9d3fd887833f08406936a1eb33d2578
barghest <bolshov03@mail.ru>
2022-06-13 22:00:53 +0000

    Add tests for modalsSlice

---
+++ src/redux/modals/modalsSlice.test.ts
@@ -0,0 +1,28 @@
+import { MODALS_INITIAL_STATE, MODALS_SLICE_NAME } from './constants';
+import modalsSlice from './modalsSlice';
+import { ModalsTypes } from './types';
+
+const state = modalsSlice.getInitialState();
+const { setModalVisability } = modalsSlice.actions;
+
+describe('modalsSlice state tests', () => {
+  test('expect set correct initial state', () => {
+    expect(modalsSlice.getInitialState()).toEqual(MODALS_INITIAL_STATE);
+  });
+  test('expect set correct slice name', () => {
+    expect(modalsSlice.name).toEqual(MODALS_SLICE_NAME);
+  });
+});
+
+describe('correct controls modalsSlice with mock action payload', () => {
+  test('expect set download modal visability truthy', () => {
+    const newState = modalsSlice.reducer(
+      state,
+      setModalVisability({ visability: true, modalType: ModalsTypes.download }),
+    );
+    expect(newState).toEqual({
+      ...state,
+      isDownloadOpen: true,
+    });
+  });
+});
