ec976d5fd0d20c7f6901811046849225fd0629ae
barghest <bolshov03@mail.ru>
2022-06-16 14:51:26 +0000

    Add tests for cart actions

--- src/redux/cart/cartSlice.test.ts
+++ src/redux/cart/cartSlice.test.ts
@@ -1,4 +1,8 @@
+import { mockDispatch } from 'test-utils/utils';
+import { waitFor } from '@testing-library/react';
+
 import { mockCartProduct } from 'test-utils/mocks';
+import * as cartApi from 'shared/api/cart';
 
 import { addProductToCart, deleteCartProduct, getCart } from './actions';
 import cartSlice from './cartSlice';
@@ -127,6 +131,98 @@ describe('correct cartSlice delete product to cart with mock action payload', ()
     expect(newState).toEqual({
       ...state,
       errors: 'error',
+    });
+  });
+});
+
+jest.mock('../../shared/api/cart');
+const mockCartApi = cartApi as jest.Mocked<typeof cartApi>;
+
+describe('resolved cart actions with async thunk', () => {
+  test('expect resolved get all cart products response', async () => {
+    const mockData = {
+      data: [mockCartProduct],
+    };
+    mockCartApi.fetchCart.mockResolvedValueOnce(mockData);
+    const cart = await mockDispatch(getCart());
+
+    await waitFor(() => {
+      expect(cart.payload).toEqual([mockCartProduct]);
+      expect(mockCartApi.fetchCart).toBeCalled();
+    });
+  });
+
+  test('expect resolved add cart product response', async () => {
+    const mockData = {
+      data: mockCartProduct,
+    };
+    mockCartApi.addProductToCart.mockResolvedValueOnce(mockData);
+    const product = await mockDispatch(addProductToCart(mockCartProduct));
+
+    await waitFor(() => {
+      expect(product.payload).toEqual(mockCartProduct);
+      expect(mockCartApi.addProductToCart).toBeCalled();
+    });
+  });
+
+  test('expect resolved delete cart product response', async () => {
+    const mockData = {
+      meta: {
+        arg: mockCartProduct,
+      },
+    };
+    mockCartApi.deleteCartProduct.mockResolvedValueOnce(mockData);
+    const product = await mockDispatch(deleteCartProduct(mockCartProduct));
+
+    await waitFor(() => {
+      expect(product.meta.arg).toEqual(mockCartProduct);
+      expect(mockCartApi.deleteCartProduct).toBeCalledWith(mockCartProduct);
+    });
+  });
+});
+
+describe('rejected cart actions with async thunk', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('expect rejected get cart products response', async () => {
+    const mockData = {
+      error: 'error',
+    };
+    mockCartApi.fetchCart.mockRejectedValue(mockData);
+    const rejectedCart = await mockDispatch(getCart());
+
+    await waitFor(() => {
+      expect(rejectedCart.payload).toEqual(mockData);
+    });
+  });
+
+  test('expect rejected add cart product response', async () => {
+    const mockData = {
+      error: 'error',
+    };
+    mockCartApi.addProductToCart.mockRejectedValue(mockData);
+    const rejectedProduct = await mockDispatch(
+      addProductToCart(mockCartProduct),
+    );
+
+    await waitFor(() => {
+      expect(rejectedProduct.payload).toEqual(mockData);
+    });
+  });
+
+  test('expect rejected delete cart product response', async () => {
+    const mockData = {
+      error: 'error',
+    };
+    mockCartApi.deleteCartProduct.mockRejectedValue(mockData);
+    const rejectedProduct = await mockDispatch(
+      deleteCartProduct(mockCartProduct),
+    );
+
+    await waitFor(() => {
+      expect(rejectedProduct.payload).toEqual(mockData);
     });
   });
 });
