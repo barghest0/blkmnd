dc71a8c762483b39d3960dc62d6a5ed89c476de4
barghest <bolshov03@mail.ru>
2022-06-17 10:27:18 +0000

    Add tests for memberships actions

--- src/redux/memberships/membershipsSlice.test.ts
+++ src/redux/memberships/membershipsSlice.test.ts
@@ -1,8 +1,11 @@
 import { mockMembership } from 'test-utils/mocks';
+import * as membershipsApi from 'shared/api/memberships';
 
 import { getMemberships } from './actions';
 import { MEMBERSHIPS_INITIAL_STATE, MEMBERSHIPS_SLICE_NAME } from './constants';
 import membershipsSlice from './membershipsSlice';
+import { mockDispatch } from 'test-utils/utils';
+import { waitFor } from '@testing-library/react';
 
 const state = membershipsSlice.getInitialState();
 
@@ -43,6 +46,42 @@ describe('correct set membershipsSlice memberships beats with mock action payloa
     expect(newState).toEqual({
       ...state,
       errors: 'error',
+    });
+  });
+});
+
+jest.mock('../../shared/api/memberships');
+const mockMembershipsApi = membershipsApi as jest.Mocked<typeof membershipsApi>;
+
+describe('resolved get memberships with async thunk', () => {
+  test('expect resolved get all memberships response', async () => {
+    const mockData = {
+      data: [mockMembership],
+    };
+    mockMembershipsApi.fetchAllMemberships.mockResolvedValueOnce(mockData);
+    const memberships = await mockDispatch(getMemberships());
+
+    await waitFor(() => {
+      expect(memberships.payload).toEqual([mockMembership]);
+      expect(mockMembershipsApi.fetchAllMemberships).toBeCalled();
+    });
+  });
+});
+
+describe('rejected get discography beats with async thunk', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('expect rejected all memberships response', async () => {
+    const mockData = {
+      error: 'error',
+    };
+    mockMembershipsApi.fetchAllMemberships.mockRejectedValue(mockData);
+    const rejectedMembership = await mockDispatch(getMemberships());
+
+    await waitFor(() => {
+      expect(rejectedMembership.payload).toEqual(mockData);
     });
   });
 });
